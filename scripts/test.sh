d1ddea045474da70701dac3be4ff8bef71b609e4
Tawhid <tawhid.mumtazshah@convoy.com>
2020-07-14 14:51:05 -0700

    remove parallel

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 set -ex
 
 jest --coverage --collectCoverageFrom='src/*.ts'
-export COVERALLS_FLAG_NAME=local
-export COVERALLS_PARALLEL=true
-export COVERALLS_SERVICE_JOB_NUMBER=211
+export COVERALLS_FLAG_NAME=unit
+# export COVERALLS_PARALLEL=true
+# export COVERALLS_SERVICE_JOB_NUMBER=211
 coveralls < coverage/lcov.info --verbose
