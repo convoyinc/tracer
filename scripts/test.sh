df7e123447d3704818344e65660e8a620e4649e7
Tawhid <tawhid.mumtazshah@convoy.com>
2020-07-14 14:15:45 -0700

    use fixed job #

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 set -ex
 
 jest --coverage --collectCoverageFrom='src/*.ts'
-export COVERALLS_FLAG_NAME=all
+export COVERALLS_FLAG_NAME=local
 export COVERALLS_PARALLEL=true
+export COVERALLS_SERVICE_JOB_NUMBER=207
 coveralls < coverage/lcov.info --verbose
