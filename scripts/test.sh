83930f43a1bec8afc8805ef4fcded35ca074fc70
Tawhid <tawhid.mumtazshah@convoy.com>
2020-07-13 20:57:27 -0700

    parallel

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,7 @@
 set -ex
 
 jest --coverage --collectCoverageFrom='src/*.ts'
-export COVERALLS_FLAG_NAME=unit
+export COVERALLS_FLAG_NAME=all
+export COVERALLS_PARALLEL=true
+export COVERALLS_REPO_TOKEN=ogKjzqSNzjP70fIkxb1BZpCrumXvsWZiv
 coveralls < coverage/lcov.info --verbose
