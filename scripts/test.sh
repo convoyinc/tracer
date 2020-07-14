ad51b2603365924b692983ae2d230ca2076161ec
Tawhid <tawhid.mumtazshah@convoy.com>
2020-07-14 14:23:03 -0700

    use 211

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ set -ex
 jest --coverage --collectCoverageFrom='src/*.ts'
 export COVERALLS_FLAG_NAME=local
 export COVERALLS_PARALLEL=true
-export COVERALLS_SERVICE_JOB_NUMBER=207
+export COVERALLS_SERVICE_JOB_NUMBER=211
 coveralls < coverage/lcov.info --verbose
